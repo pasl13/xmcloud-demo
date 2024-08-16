137828eb4c5ef7c55824c229cc96da1cf77eb5f0
Jonatas Ribeiro <jonatas.m.ribeiro@noesis.pt>
2024-08-16 11:53:21 +0100

    testing component state management response and behavior on serverside components

--- src/sxastarter/src/components/CookiesTab.tsx
+++ src/sxastarter/src/components/CookiesTab.tsx
@@ -1,19 +1,54 @@
-import React from 'react';
+// 'use client';
+import React, { useState } from 'react';
 import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
+
+type FieldObject = {
+  value: string;
+};
+
+type FieldType = {
+  Title: FieldObject;
+  Description: FieldObject;
+};
 
 interface CookiesTabProps {
   rendering: ComponentRendering & { params: ComponentParams };
   params: ComponentParams;
+  fields: FieldType;
 }
 
 export const Default = (props: CookiesTabProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
+  console.log('CookiesTabProps:', props);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleIconClick = () => {
+    console.log('handleIconClick', isOpen);
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  console.log('isOpen:', isOpen);
 
   return (
-    <div className={`component ${props.params.styles}`} id={id ? id : undefined}>
-      <div className="component-content">
-        <p>CookiesTab Component</p>
+    <>
+      <div className={`component ${props.params.styles}`} id={id ? id : undefined}>
+        <div className="component-content">
+          <p>{props.fields.Title.value}</p>
+          <button onClick={handleIconClick}>Open Modal</button>
+        </div>
       </div>
-    </div>
+      {isOpen && (
+        <div>
+          <div>
+            <p>This is the modal content</p>
+            <button onClick={handleCloseModal}>Close Modal</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
