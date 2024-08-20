adfd98049f70161f27c6697783585019384f7d59
Jonatas Ribeiro <jonatas.m.ribeiro@noesis.pt>
2024-08-20 11:19:50 +0100

    CookieSettings buiding up component return statement & localState management for modal opening control

--- src/sxastarter/src/atoms/Shared Components/CookieModal.tsx
+++ src/sxastarter/src/atoms/Shared Components/CookieModal.tsx
@@ -8,11 +8,7 @@ interface CookieModalProps {
   handleClose: () => void;
 }
 
-const CookieModal = ({
-  children,
-  isOpen,
-  handleClose
-}: CookieModalProps) => {
+const CookieModal = ({ children, isOpen, handleClose }: CookieModalProps) => {
   useEffect(() => {
     const closeOnEscapeKey = (e: KeyboardEvent) => (e.key === 'Escape' ? handleClose() : null);
     document.body.addEventListener('keydown', closeOnEscapeKey);
@@ -33,6 +29,18 @@ const CookieModal = ({
     <ReactPortal wrapperId="react-portal-modal-container">
       <>
         <div className="fixed top-0 left-0 w-screen h-screen z-40 bg-neutral-800 opacity-50" />
+        <div
+          className="fixed rounded flex flex-col box-border min-w-fit overflow-hidden p-5 bg-zinc-800 inset-y-32 
+        inset-x-1"
+        >
+          <button
+            onClick={handleClose}
+            className="py-2 px-8 self-end font-bold hover:bg-violet-600 border rounded"
+          >
+            close
+          </button>
+          <div className="box-border h-5/6">{children}</div>
+        </div>
       </>
     </ReactPortal>
   );
--- src/sxastarter/src/components/CookiesSettings.tsx
+++ src/sxastarter/src/components/CookiesSettings.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
+import CookieModal from 'src/atoms/Shared Components/CookieModal';
 
 interface CookiesSettingsProps {
   rendering: ComponentRendering & { params: ComponentParams };
@@ -8,12 +10,33 @@ interface CookiesSettingsProps {
 
 export const Default = (props: CookiesSettingsProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <div className={`component ${props.params.styles}`} id={id ? id : undefined}>
       <div className="component-content">
-        <p className='border-red-500'>CookiesSettings Component</p>
+        <p className="">CookiesSettings Component</p>
+        <button onClick={() => setModalOpen(true)}>Open Modal</button>
       </div>
+      {modalOpen && (
+        <CookieModal isOpen={modalOpen} handleClose={() => setModalOpen(!modalOpen)}>
+          <div className="flex flex-col justify-between h-full w-full">
+            <div className="flex flex-col mt-auto mb-auto items-center p-8">
+              <span>Close Modal?</span>
+              <span>All current progress will be lost</span>
+            </div>
+            <div className="flex flex-col sm:flex-row justify-center gap-8 align-center">
+              <button
+                className="py-2 px-8 font-bold hover:bg-violet-600 border rounded"
+                type="button"
+                onClick={() => setModalOpen(!modalOpen)}
+              >
+                Continue Working
+              </button>
+            </div>
+          </div>
+        </CookieModal>
+      )}
     </div>
   );
 };
