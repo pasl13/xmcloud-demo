e188b3b9594bff098bd3b68a75f6d34f00688b69
Williker Carmo <williker.c.carmo@noesis.pt>
2024-08-31 11:16:56 +0100

    Refactor GovernmentManagement Component

--- src/sxastarter/src/components/government-management/GovernmentManagement.tsx
+++ src/sxastarter/src/components/government-management/GovernmentManagement.tsx
@@ -1,65 +1,25 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { ApolloProvider } from '@apollo/client';
-import {
-  Tabs,
-  Tab,
-  Dropdown,
-  DropdownTrigger,
-  Button,
-  DropdownMenu,
-  DropdownItem,
-  useDisclosure,
-  Modal,
-  ModalContent,
-  ModalBody,
-} from '@nextui-org/react';
-import moment from 'moment';
-import { FaChevronDown, FaPlus } from 'react-icons/fa';
-
-import { ComponentParams, ComponentRendering, TextField } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
 import client from 'src/config/apolloClient';
-import AddOfficialForm from './AddOfficial';
+import { Tab, Tabs } from '@nextui-org/react';
 import AddConstitutionalGovernment from './AddConstitutionalGovernment';
 import AddPrimeMinister from './AddPrimeMinister';
 
-type ResultsFieldText = {
-  id: string;
-  field: {
-    jsonValue: TextField;
-  };
-};
-
-type ResultsSexItems = {
-  id: string;
-  displayName: string;
-};
-
-type ResultsConstitutionalGovernment = {
+interface ResultsGovernment {
   id: string;
   name: string;
   field: {
     value: string;
   };
-};
+}
 
 interface Fields {
   data: {
     datasource: {
-      language: {
-        name: string;
-      };
+      hasChildren: boolean;
       children: {
-        results: ResultsFieldText[];
-      };
-    };
-    sexItems: {
-      children: {
-        results: ResultsSexItems[];
-      };
-    };
-    getConstitutionalGovernments: {
-      children: {
-        results: ResultsConstitutionalGovernment[];
+        results: ResultsGovernment[];
       };
     };
   };
@@ -71,69 +31,44 @@ interface GovernmentManagementProps {
   fields: Fields;
 }
 
-export const Default = ({ rendering, params, fields }: GovernmentManagementProps): JSX.Element => {
+const sortGovernments = (a: ResultsGovernment, b: ResultsGovernment): number => {
+  if (a.field.value === '1') return -1;
+  if (b.field.value === '1') return 1;
+  return b.name.localeCompare(a.name);
+};
+
+const GovernmentManagement = ({
+  params,
+  fields,
+}: GovernmentManagementProps): JSX.Element => {
   const { RenderingIdentifier: id } = params;
-  const { dataSource: parent } = rendering;
-  const { name: language } = fields.data.datasource.language;
-  const sexItems = fields.data.sexItems.children.results;
+  const { hasChildren, children } = fields.data.datasource;
 
-  const constitutionalGovernments = useMemo(
-    () =>
-      fields.data.getConstitutionalGovernments.children.results.sort((a, b) => {
-        const dateA = a.field.value
-          ? moment(a.field.value, 'YYYYMMDDTHHmmssZ').toDate().getTime()
-          : Infinity;
-        const dateB = b.field.value
-          ? moment(b.field.value, 'YYYYMMDDTHHmmssZ').toDate().getTime()
-          : Infinity;
-        return dateB - dateA;
-      }),
-    [fields]
-  );
+  const constitutionalGovernments = useMemo<ResultsGovernment[]>(() => {
+    if (!hasChildren) return [];
+    return children.results.sort(sortGovernments);
+  }, [hasChildren, children]);
 
-  const officialResults = fields.data.datasource.children.results || [];
-  const officialsList = officialResults.map(({ id, field }) => ({
-    key: id,
-    name: field.jsonValue?.value?.toString() || '',
-  }));
-
-  const [selectedOfficialId, setSelectedOfficialId] = useState<string>('');
   const [selectedGovernmentId, setSelectedGovernmentId] = useState<string>(
     constitutionalGovernments[0]?.id || ''
   );
   const [selectedGovernmentTab, setSelectedGovernmentTab] = useState<string>(
     'constitutional-management'
   );
-  const [officials, setOfficials] = useState(officialsList);
-  const [itemId, setItemId] = useState('');
-  const [title, setTitle] = useState('');
-  const [titleEn, setTitleEn] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
-  useEffect(() => {
-    setOfficials(officialsList);
-  }, [officialResults]);
-
-  const handleOfficialSelect = (officialId: string) => {
-    setSelectedOfficialId(officialId);
-  };
-
-  const handleAddOfficial = (officialId: string, officialName: string) => {
-    setOfficials((prevOfficials) => [...prevOfficials, { key: officialId, name: officialName }]);
-    onOpenChange();
-  };
+  const [governmentDetails, setGovernmentDetails] = useState({
+    itemId: '',
+    title: '',
+    titleEn: '',
+    startDate: '',
+  });
 
   const handleAddGovernment = (
     itemId: string,
     title: string,
     titleEn: string,
     startDate: string
-  ) => {
-    setItemId(itemId);
-    setTitle(title);
-    setTitleEn(titleEn);
-    setStartDate(startDate);
+  ): void => {
+    setGovernmentDetails({ itemId, title, titleEn, startDate });
     setSelectedGovernmentTab('prime-minister');
   };
 
@@ -142,78 +77,31 @@ export const Default = ({ rendering, params, fields }: GovernmentManagementProps
       <div className={`component government-management ${params.styles}`} id={id}>
         <div className="component-content space-y-4">
           <h1 className="font-bold text-gray-800">Government Management</h1>
-          <div className="flex items-center space-x-4">
-            <Dropdown>
-              <DropdownTrigger>
-                <Button className="bg-gray-200 text-gray-700 font-semibold py-2 px-4 rounded-md hover:bg-gray-300 transition duration-300 ease-in-out">
-                  {selectedOfficialId
-                    ? officials.find((official) => official.key === selectedOfficialId)?.name
-                    : '-- Select an official --'}
-                  <FaChevronDown className="ml-2" />
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                aria-label="Select an official"
-                selectionMode="single"
-                selectedKeys={selectedOfficialId ? new Set([selectedOfficialId]) : new Set()}
-                onSelectionChange={(keys) => handleOfficialSelect(Array.from(keys).join(''))}
-              >
-                {officials.map((official) => (
-                  <DropdownItem key={official.key}>{official.name}</DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-
-            <Button
-              onPress={onOpen}
-              className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 ease-in-out"
-            >
-              <FaPlus className="mr-2" />
-              Add Official
-            </Button>
-          </div>
-          <Modal isOpen={isOpen} onOpenChange={onOpenChange} className="w-full max-w-3xl">
-            <ModalContent>
-              <>
-                <ModalBody>
-                  <AddOfficialForm
-                    onAddOfficial={handleAddOfficial}
-                    parent={parent}
-                    language={language}
-                    sexItems={sexItems}
-                  />
-                </ModalBody>
-              </>
-            </ModalContent>
-          </Modal>
-
           <Tabs
             aria-label="Government Tabs"
             selectedKey={selectedGovernmentId}
             onSelectionChange={(key) => setSelectedGovernmentId(key as string)}
-            className="flex justify-start space-x-4 border-b-2 border-gray-200 mb-4"
           >
             {constitutionalGovernments.map((gov) => (
               <Tab key={gov.id} title={gov.name}>
-                <div>{`Content for ${gov.name}`}</div>
+                <div>
+                  <p>{`Content for ${gov.name}`}</p>
+                </div>
               </Tab>
             ))}
             <Tab key="add-new" title="+ Add New">
-              <div className="p-4"></div>
-            </Tab>
-          </Tabs>
-
-          <Tabs
-            aria-label="Government Management Tabs"
-            selectedKey={selectedGovernmentTab}
-            onSelectionChange={(key) => setSelectedGovernmentTab(key as string)}
-            className="flex justify-start space-x-4 border-b-2 border-gray-200 mb-4"
-          >
-            <Tab key="constitutional-management" title="Constitutional Management">
-              <AddConstitutionalGovernment onAddGovernment={handleAddGovernment} />
-            </Tab>
-            <Tab key="prime-minister" title="Prime Minister">
-              <AddPrimeMinister itemId={itemId} title={title} titleEn={titleEn} startDate={startDate} />
+              <Tabs
+                aria-label="Government Management Tabs"
+                selectedKey={selectedGovernmentTab}
+                onSelectionChange={(key) => setSelectedGovernmentTab(key as string)}
+              >
+                <Tab key="constitutional-management" title="Constitutional Management">
+                  <AddConstitutionalGovernment onAddGovernment={handleAddGovernment} />
+                </Tab>
+                <Tab key="prime-minister" title="Prime Minister">
+                  <AddPrimeMinister {...governmentDetails} />
+                </Tab>
+              </Tabs>
             </Tab>
           </Tabs>
         </div>
@@ -221,3 +109,5 @@ export const Default = ({ rendering, params, fields }: GovernmentManagementProps
     </ApolloProvider>
   );
 };
+
+export default GovernmentManagement;
