7ade03084d900a2a10b7a1e73eaa24dc2463d2e8
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-09-11 14:21:01 +0100

    SimplePieChart implementation and adjustment & SimpleDonutChart implementation

--- src/sxastarter/src/components/SimpleBarChart.tsx
+++ src/sxastarter/src/components/SimpleBarChart.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { AgCharts } from 'ag-charts-react';
 import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
