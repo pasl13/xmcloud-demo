f185da9022b957c7f40ab2104e5a319806a2a49e
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-09-17 12:32:54 +0100

    tailwind config with agora

---
+++ src/sxastarter/tailwind.config.ts
@@ -0,0 +1,28 @@
+// const {nextui} = require("@nextui-org/react");
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [nextui()],
+// }
+
+import { AgoraTailwindConfig } from '@ama-pt/agora-design-system';
+import type { Config } from 'tailwindcss';
+
+const TailwindConfig: Config = {
+  content: ['src/**/*.{ts,tsx}'],
+  theme: AgoraTailwindConfig.theme,
+  plugins: AgoraTailwindConfig.plugins,
+  safelist: AgoraTailwindConfig.safelist,
+  corePlugins: {
+    preflight: false,
+  },
+};
+
+export default TailwindConfig;
