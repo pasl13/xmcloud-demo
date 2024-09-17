c5aafcfb0984649cfc7b3679019cc93dd54828e2
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-09-17 12:27:43 +0100

    installing and configuring tailwind

--- src/sxastarter/tailwind.config.js
+++ src/sxastarter/tailwind.config.js
@@ -1,15 +1,15 @@
-// const {nextui} = require("@nextui-org/react");
+const {nextui} = require("@nextui-org/react");
 
 
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [nextui()],
-// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [nextui()],
+}
 
