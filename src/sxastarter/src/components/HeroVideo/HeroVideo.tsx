ae8ccfb0e4d4d4d3ddb701a3f044782229b0fa27
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-09-27 16:42:44 +0100

    [TASK 103] - final version of hero video component

--- src/sxastarter/src/components/HeroVideo/HeroVideo.tsx
+++ src/sxastarter/src/components/HeroVideo/HeroVideo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 import React, { useEffect, useState } from 'react';
 // import ReactPlayer from 'react-player';
@@ -24,7 +25,7 @@ interface HeroVideoFields {
   };
 }
 
-export const Default = (props: HeroVideoProps): JSX.Element => {
+export const Default = (props: HeroVideoProps): JSX.Element | null => {
   const id = props.params.RenderingIdentifier;
   const [fields, setFields] = useState<any[]>([]);
   const [hydrated, setHydrated] = useState(false);
@@ -38,21 +39,33 @@ export const Default = (props: HeroVideoProps): JSX.Element => {
     return null;
   }
 
-  const heroVideoSource = fields.find(f => f.name === "HeroVideoSource")?.jsonValue.value.href || "";
-  const heroVideoTitle = fields.find(f => f.name === "HeroVideoTitle")?.jsonValue.value || "";
-  const heroVideoAlt = fields.find(f => f.name === "HeroVideoAlt")?.jsonValue.value || "";
-  const heroVideoAutoplay = fields.find(f => f.name === "HeroVideoAutoplay")?.jsonValue.value || false;
-  const heroVideoLoop = fields.find(f => f.name === "HeroVideoLoop")?.jsonValue.value || false;
-  const heroVideoMute = fields.find(f => f.name === "HeroVideoMute")?.jsonValue.value || false;
-  const heroVideoOverlay = fields.find(f => f.name === "HeroVideoOverlay")?.jsonValue.value || false;
-  const heroVideoFallbackImage = fields.find(f => f.name === "HeroVideoFallbackImage")?.jsonValue.value || {};
-
+  // const heroVideoSource = fields.find(f => f.name === "HeroVideoSource")?.jsonValue.value.href || "";
+  const heroVideoSource = (() => {
+    const videoField = fields.find((f) => f.name === 'HeroVideoSource')?.jsonValue.value;
+    if (typeof videoField === 'string') {
+      return videoField;
+    } else if (videoField && typeof videoField === 'object' && 'href' in videoField) {
+      return videoField.href;
+    }
+    return '';
+  })();
+  const heroVideoTitle = fields.find((f) => f.name === 'HeroVideoTitle')?.jsonValue.value || '';
+  const heroVideoAlt = fields.find((f) => f.name === 'HeroVideoAlt')?.jsonValue.value || '';
+  const heroVideoAutoplay =
+    fields.find((f) => f.name === 'HeroVideoAutoplay')?.jsonValue.value || false;
+  const heroVideoLoop = fields.find((f) => f.name === 'HeroVideoLoop')?.jsonValue.value || false;
+  const heroVideoMute = fields.find((f) => f.name === 'HeroVideoMute')?.jsonValue.value || false;
+  const heroVideoOverlay =
+    fields.find((f) => f.name === 'HeroVideoOverlay')?.jsonValue.value || false;
+  const heroVideoFallbackImage =
+    fields.find((f) => f.name === 'HeroVideoFallbackImage')?.jsonValue.value || {};
   return (
     <>
-      <div className="relative w-full h-0 pb-[56.25%]">
-        {heroVideoOverlay && (
-          <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
-        )}
+      <div
+        className="relative w-full h-0 pb-[56.25%] component ${props.params.styles}}"
+        id={id ? id : undefined}
+      >
+        {heroVideoOverlay && <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>}
         {heroVideoSource ? (
           <video
             className="absolute inset-0 w-full h-full object-cover"
@@ -64,12 +77,16 @@ export const Default = (props: HeroVideoProps): JSX.Element => {
             muted={heroVideoMute}
             poster={heroVideoFallbackImage?.src}
           />
-        ) : (
+        ) : heroVideoFallbackImage.src ? (
           <img
             className="absolute inset-0 w-full h-full object-cover"
             src={heroVideoFallbackImage.src}
             alt={heroVideoFallbackImage.alt || 'Video fallback'}
           />
+        ) : (
+          <div className="absolute inset-0 w-full h-full flex items-center justify-center bg-gray-200">
+            <h2>No image</h2>
+          </div>
         )}
       </div>
     </>
