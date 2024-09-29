28e1cd3b4cbd732f5a18c94a0f0d72b896ffb404
JoaoAfonsoAlmeida <joao.a.almeida@noesis.pt>
2024-09-29 03:03:10 +0100

    Banner variant

--- src/sxastarter/src/components/Banner/Banner.tsx
+++ src/sxastarter/src/components/Banner/Banner.tsx
@@ -3,32 +3,50 @@ import { BannerModel } from 'src/models/BannerModel';
 import { RichText as JssRichText, Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
 
 const Banner = (props: BannerModel): JSX.Element => {
+  console.log(props.fields.Image);
   return (
-    <div className="component banner">
+    <div
+      className="component banner"
+      style={{ backgroundImage: `url(${props.fields.Image?.value?.src})` }}
+    >
       <div className="component-content">
-        <div className="banner-breadcrump">
-          {/* BreadCrump Section */}
+        <div className="banner-breadcrumb">
           <Placeholder name="breadcrumb-placeholder" rendering={props.rendering} />
         </div>
         <div>{props.fields.LectureTime.value}</div>
         <div className="banner-field-title">
-          {/* Section used for "O OE205... para os Empresas" */}
           <JssRichText field={props.fields.Title} />
         </div>
         <div className="banner-field-text">
-          {/* Section used for "Este é um orçamento... negativos nos seus orçamentos." */}
           <JssRichText field={props.fields.Text} />
         </div>
-        <div className="banner-field-updatemessage">
-          {/* Section used for "Atualizado em 13.09.2024" */}
-          {props.fields.UpdateMessage.value}
-        </div>
-      </div>
-      <div className="banner-field-image">
-        <Image field={props.fields.Image} alt="Alt Image" />
+        <div className="banner-field-updatemessage">{props.fields.UpdateMessage.value}</div>
       </div>
     </div>
   );
 };
 
 export default Banner;
+
+export const withImageBySide = (props: BannerModel): JSX.Element => {
+  return (
+    <div className="component banner">
+      <div className="component-content">
+        <div className="banner-breadcrumb">
+          <Placeholder name="breadcrumb-placeholder" rendering={props.rendering} />
+        </div>
+        <div>{props.fields.LectureTime.value}</div>
+        <div className="banner-field-title">
+          <JssRichText field={props.fields.Title} />
+        </div>
+        <div className="banner-field-text">
+          <JssRichText field={props.fields.Text} />
+        </div>
+        <div className="banner-field-updatemessage">{props.fields.UpdateMessage.value}</div>
+      </div>
+      <div className="banner-field-image">
+        <Image field={props.fields.Image} />
+      </div>
+    </div>
+  );
+};
