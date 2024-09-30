ce03fda1f8d10d54e1da24699026298bf855fdbc
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-09-30 23:22:54 +0100

    [TASK 116] - with title ternal condition & render

--- src/sxastarter/src/components/TextEffectManagement/CounterEffect.tsx
+++ src/sxastarter/src/components/TextEffectManagement/CounterEffect.tsx
@@ -1,77 +1,3 @@
-// import React, { useEffect, useRef, useState } from 'react';
-// import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
-// import { CountUp } from 'countup.js';
-
-// interface CounterField<T> {
-//   value: T;
-// }
-// interface CounterEffectProps {
-//   rendering: ComponentRendering & { params: ComponentParams };
-//   params: ComponentParams;
-//   fields: {
-//     counterTargetNumber: CounterField<number>;
-//     counterPrefix: CounterField<string>;
-//     counterSuffix: CounterField<string>;
-//     counterDuration: CounterField<string>; // Since it seems to be a string from your example
-//     counterAutoplayOption: CounterField<boolean>;
-//   };
-// }
-
-// export const Default = (props: CounterEffectProps): JSX.Element => {
-//   const id = props.params.RenderingIdentifier;
-//   // const [fields, setFields] = useState([]);
-//   // const [hydrated, setHydrated] = useState(false);
-//   const fields = props.fields;
-//   const counterRef = useRef(null);
-//   console.log('CounterEffectProps', props);
-
-//   // useEffect(() => {
-//   //   if (props.fields && !hydrated) {
-//   //     setFields(props.fields);
-//   //     setHydrated(true);
-//   //   }
-//   // }, [props.fields, hydrated]);
-
-//   const targetNumber = fields.counterTargetNumber.value;
-//   const prefix = fields.counterPrefix.value;
-//   const suffix = fields.counterSuffix.value;
-//   const duration = parseFloat(fields.counterDuration.value); // Convert string duration to number
-//   const autoplay = fields.counterAutoplayOption.value;
-
-//   useEffect(() => {
-//     if (counterRef.current) {
-//       const countUp = new CountUp(counterRef.current, targetNumber, {
-//         prefix: '€',
-//         duration: 2, // Adjust the duration as needed
-//       });
-//       if (!countUp.error) {
-//         countUp.start();
-//       } else {
-//         console.error(countUp.error);
-//       }
-//     }
-//   }, [targetNumber]);
-
-//   return (
-//     <div className={`component ${props.params.styles}`} id={id ? id : undefined}>
-//       <div className="component-content">
-//         <div>
-//           <h1>Title Here</h1>
-//           {/* Render the Rich Text but with a span for the number */}
-//           {/* <div
-//             dangerouslySetInnerHTML={{
-//               __html: richText.replace(
-//                 /\d+/, // Assuming the number is identifiable and easy to replace
-//                 `<span id="counter" ref=${counterRef}>0</span>`
-//               ),
-//             }}
-//           /> */}
-//           <div>{targetNumber}</div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 'use client';
 
 import React, { useEffect, useRef } from 'react';
@@ -89,8 +15,9 @@ interface CounterEffectProps {
     counterTargetNumber: CounterField<number>;
     counterPrefix: CounterField<string>;
     counterSuffix: CounterField<string>;
-    counterDuration: CounterField<string>; // Since it seems to be a string from your example
+    counterDuration: CounterField<string>;
     counterAutoplayOption: CounterField<boolean>;
+    counterTitleText: CounterField<string>;
   };
 }
 
@@ -102,8 +29,9 @@ export const Default = (props: CounterEffectProps): JSX.Element => {
   const targetNumber = fields.counterTargetNumber.value;
   const prefix = fields.counterPrefix.value;
   const suffix = fields.counterSuffix.value;
-  const duration = parseFloat(fields.counterDuration.value); // Convert string duration to number
+  const duration = parseFloat(fields.counterDuration.value);
   const autoplay = fields.counterAutoplayOption.value;
+  const titleText = fields.counterTitleText.value;
 
   useEffect(() => {
     if (counterRef.current) {
@@ -120,8 +48,17 @@ export const Default = (props: CounterEffectProps): JSX.Element => {
   }, [targetNumber, prefix, suffix, duration, autoplay]);
 
   return (
-    <div id={id ? id : undefined}>
-      <span ref={counterRef}>0</span>
+    <div className={`component ${props.params.styles}`} id={id ? id : undefined}>
+      {titleText ? (
+        <div className="component-content-with-title">
+          <h2>{titleText}</h2>
+          <span ref={counterRef}>0</span>
+        </div>
+      ) : (
+        <div className="component-content-counter">
+          <span ref={counterRef}>0</span>
+        </div>
+      )}
     </div>
   );
 };
