28e497e7868667537d62078b1989466865e3ce78
Williker Carmo <70776059+willikercarmo@users.noreply.github.com>
2025-01-28 10:42:22 +0000

    Add CustomNavigationContentResolver

---
+++ src/platform/Feature/Navigation/ContentsResolvers/CustomNavigationContentResolver.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using Sitecore.Data.Fields;
+using Sitecore.XA.JSS.Feature.Navigation.ContentsResolvers;
+using Sitecore.XA.JSS.Feature.Navigation.Models;
+using System.Collections.Generic;
+
+namespace XmCloudSXAStarter.Feature.Navigation.ContentsResolvers
+{
+    public class CustomNavigationContentResolver : NavigationContentResolver
+    {
+        protected override void AddFields(
+            NavigationItemModel navigationItem,
+            JObject navigationChildNode,
+            IEnumerable<string> fields)
+        {
+            base.AddFields(navigationItem, navigationChildNode, fields);
+
+            Field pageDescriptionField = navigationItem.Item.Fields["PageDescription"];
+            if (pageDescriptionField != null && !string.IsNullOrEmpty(pageDescriptionField.Value))
+            {
+                navigationChildNode["PageDescription"] = pageDescriptionField.Value;
+            }
+        }
+    }
+}
